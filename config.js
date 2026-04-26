de8a5974dd869685f46a31047fd8b72a267bd15c
PazeYellow <131520020+PazeYellow@users.noreply.github.com>
2026-04-26 13:42:51 +0000

    Deploying to gh-pages from @ PazeYellow/Webapp@e41e307391beccd0ceb5d94e73e1a64d309b720f 🚀

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    BIN_ID: "69eda6feaaba8821973af658",
-    API_KEY: "a0.tryboPhC1.o.h./sTrxQkfjW7Wd.JI00IimnkA47zHa",
-    ACCESS_KEY: "a0$/CakmC8cloGjylWos2NU7.TRMKim5/l7Dkqkeozteb7Mo5LYjzLq6",
-    ADMIN_PASS: "CustomYUGIOHBanlist12"
+    "BIN_ID": "69eda6feaaba8821973af658",
+    "API_KEY": "$2a$10$eVCBs2gFb.tryboPhC1.o.h./sTrxQkfjW7Wd.JI00IimnkA47zHa",
+    "ACCESS_KEY": "$2a$10$/CakmC8cloGjylWos2NU7.TRMKim5/l7Dkqkeozteb7Mo5LYjzLq6",
+    "ADMIN_PASS": "CustomYUGIOHBanlist12"
 };
