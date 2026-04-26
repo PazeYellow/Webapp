fbbaedbb594a733f9a86d1968837294fbdebae1d
PazeYellow <131520020+PazeYellow@users.noreply.github.com>
2026-04-26 16:42:54 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 const CONFIG = {
     BIN_ID: "69eda6feaaba8821973af658",
-
-    // MASTER KEY
     API_KEY: "$2a$10$eVCBs2gFb.tryboPhC1.o.h./sTrxQkfjW7Wd.JI00IimnkA47zHa",
-
-    // ACCESS KEY (NEW)
     ACCESS_KEY: "$2a$10$/CakmC8cloGjylWos2NU7.TRMKim5/l7Dkqkeozteb7Mo5LYjzLq6",
-
     ADMIN_PASS: "CustomYUGIOHBanlist123"
 };
