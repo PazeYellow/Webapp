dad6f5565e96d678827074b1a2039b17fe64bc14
PazeYellow <131520020+PazeYellow@users.noreply.github.com>
2026-04-26 16:19:27 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,5 @@
-const _0xData = {
-    // Your Bin ID
-    k1: "69eda6feaaba8821973af658", 
-    // Your Key: $2a$10$eVCBs2gFb.tryboPhC1.o.h./sTrxQkfjW7Wd.JI00IimnkA47zHa
-    k2: "JDJhJDEwJGVWQ0JzMmdGYi50cnlib1BoQzEuby5oLi9zVHJ4UWtmalclV2QuSkkwMElpbW5rQTQ3ekhh", 
-    // Your Password: CustomYUGIOHBanlist123
-    k3: "Q3VzdG9tWVVHSU9IQmFubGlzdDEyMw==" 
-};
-
 const CONFIG = {
-    BIN_ID: _0xData.k1,
-    API_KEY: atob(_0xData.k2),
-    ADMIN_PASS: atob(_0xData.k3)
+    BIN_ID: "69eda6feaaba8821973af658",
+    API_KEY: atob("JDJhJDEwJGVWQ0JzMmdGYi50cnlib1BoQzEuby5oLi9zVHJ4UWtmalclV2QuSkkwMElpbW5rQTQ3ekhh"),
+    ADMIN_PASS: atob("Q3VzdG9tWVVHSU9IQmFubGlzdDEyMw==")
 };
