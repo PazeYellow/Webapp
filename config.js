cff4af2ab288bbe66feda2dbe763728e98c0d4d5
PazeYellow <131520020+PazeYellow@users.noreply.github.com>
2026-04-26 16:10:51 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 const _0xData = {
+    // Your Bin ID
     k1: "69eda6feaaba8821973af658", 
+    // Your Key: $2a$10$eVCBs2gFb.tryboPhC1.o.h./sTrxQkfjW7Wd.JI00IimnkA47zHa
     k2: "JDJhJDEwJGVWQ0JzMmdGYi50cnlib1BoQzEuby5oLi9zVHJ4UWtmalclV2QuSkkwMElpbW5rQTQ3ekhh", 
+    // Your Password: CustomYUGIOHBanlist123
     k3: "Q3VzdG9tWVVHSU9IQmFubGlzdDEyMw==" 
 };
 
