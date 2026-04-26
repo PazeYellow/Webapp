eb490ddd697eac00eadcfeb32e76bd4fc46c21e4
PazeYellow <131520020+PazeYellow@users.noreply.github.com>
2026-04-26 16:05:43 +1000

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const _0xData = {
+    k1: "69eda6feaaba8821973af658", 
+    k2: "JDJhJDEwJGVWQ0JzMmdGYi50cnlib1BoQzEuby5oLi9zVHJ4UWtmalclV2QuSkkwMElpbW5rQTQ3ekhh", 
+    k3: "Q3VzdG9tWVVHSU9IQmFubGlzdDEyMw==" 
+};
+
+const CONFIG = {
+    BIN_ID: _0xData.k1,
+    API_KEY: atob(_0xData.k2),
+    ADMIN_PASS: atob(_0xData.k3)
+};
